67801a89ff68b2a01e08020b79ec5aea660c3ee3
Serxhjo Shahini <sshahini@2-H-6.42heilbronn.de>
2024-06-07 21:37:04 +0200

    atoi included in libft

--- minitalk.h
+++ minitalk.h
@@ -6,7 +6,7 @@
 /*   By: sshahini <sshahini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/07 21:00:47 by sshahini          #+#    #+#             */
-/*   Updated: 2024/06/07 21:30:22 by sshahini         ###   ########.fr       */
+/*   Updated: 2024/06/07 21:36:38 by sshahini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,8 @@
 # include <stdio.h>
 # include <unistd.h>
 
-int		ft_atoi(const char *str);
 void	send_bit(int pid, char c);
 void	sig_handler(int sig);
 void	convert_to_char(int sig, siginfo_t *info, void *context);
-
 
 #endif
