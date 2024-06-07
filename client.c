3903ca3b18146357278997fb55af407bb2ab497b
Serxhjo Shahini <sshahini@2-H-6.42heilbronn.de>
2024-06-07 22:08:24 +0200

    client.c finished, sucesfully run a test where the server and client communicated

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: sshahini <sshahini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/15 12:06:30 by sshahini          #+#    #+#             */
-/*   Updated: 2024/06/07 21:22:59 by sshahini         ###   ########.fr       */
+/*   Updated: 2024/06/07 21:40:49 by sshahini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,4 +32,26 @@ void	sig_handler(int sig)
 {
 	if (sig == SIGUSR1)
 		printf("The server has successfully received the message!\n");
+}
+
+int	main(int argc, char *argv[])
+{
+	int	pid;
+	int	i;
+
+	i = 0;
+	if (argc != 3)
+	{
+		printf("Error: Invalid number of arguments\n");
+		return (1);
+	}
+	signal(SIGUSR1, sig_handler);
+	pid = ft_atoi(argv[1]);
+	while (argv[2][i])
+	{
+		send_bit(pid, argv[2][i]);
+		i++;
+	}
+	send_bit(pid, '\0');
+	return (0);
 }
